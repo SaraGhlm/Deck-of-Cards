c3402ffa388f191bc52b69f6ff15c62c0b874d51
Sara Gholami <srghlm6@gmail.com>
2020-04-10 20:24:31 +0000

    Initial commit

---
+++ Deck.java
@@ -0,0 +1,28 @@
+/**
+ * Deck.java
+ * 
+ * @brief A class that represents a deck of playing cards
+ * 
+ * @details
+ * This class is responsible for shuffling and dealing a deck of cards. The deck should
+ * contain 52 cards A,2 - 10, J,Q K or four suits, but no jokers.
+ */
+class Deck 
+{
+  
+  public Deck() 
+  {}
+  
+  public void deal_hand( int sets, int cards ) 
+  {}
+  
+  public void print_deck() 
+  {}
+  
+  public void shuffle( int seed ) 
+  {}
+  
+  public void sort() 
+  {}
+  
+}
