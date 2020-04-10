eff41bb1eb0ce50cee980e9e0e166443fc2f64c4
Sara Gholami <srghlm6@gmail.com>
2020-04-10 20:30:28 +0000

    completed the Deck constructor to initialize the cards

--- Deck.java
+++ Deck.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Deck.java
  * 
@@ -9,9 +11,21 @@
  */
 class Deck 
 {
-  
-  public Deck() 
-  {}
+  private ArrayList<Card> all_cards; // ArrayList of all 52 cards
+
+  // Deck constructor which initializes the all_cards list
+  public Deck() {
+    all_cards = new ArrayList<>();
+
+    // Initializing the all_cards array
+    for (int suit = 0; suit < 4; suit++) {
+      for (int value = 1; value < 14; value++) { // Excluding the joker as a value
+        Card card = new Card();
+        card.set_value(value, suit);
+        all_cards.add(card);
+      }
+    }
+  }
   
   public void deal_hand( int sets, int cards ) 
   {}
