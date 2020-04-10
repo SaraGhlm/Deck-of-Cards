4eca5472412797607724876a315080f238908943
Sara Gholami <srghlm6@gmail.com>
2020-04-10 21:25:24 +0000

    completed sort function, which uses merge sort. Added a compare function to the card class

--- Card.java
+++ Card.java
@@ -10,17 +10,15 @@
 class Card {
   
   // Suit enumeration
-  public enum Suit 
-  {
-    SPADES,
-    HEARTS,
+  public enum Suit {
     DIAMONDS,
-    CLUBS
+    CLUBS,
+    HEARTS,
+    SPADES
   }
   
   // Value enumeration
-  public enum Value 
-  {
+  public enum Value {
     JOKER,
     ACE,
     TWO,
@@ -44,17 +42,33 @@ class Card {
   // Returns the suit of the card.
 	//
 	// @return	The suit.
-  public Suit get_suit() 
-  {
+  public Suit get_suit() {
     return m_suit;
   }
   
   // Returns the value of the card.
 	//
 	// @return	The suit enumeration.
-  public Value get_value() 
-  {
+  public Value get_value() {
     return m_value;
+  }
+
+  // Compares if the card suit of the left card is less than the right.
+  // Does not take value into consideration.
+  //
+  // @param[in] rhs   The right hand side card.
+  // @return    true if less than.
+  public boolean suit_is_less_than( Card rhs ) {
+    return m_suit.ordinal() < rhs.m_suit.ordinal();
+  }
+
+  // Compares if the card suit of the left card is greater than the right.
+  // Does not take value into consideration.
+  //
+  // @param[in] rhs   The right hand side card.
+  // @return    true if greater than.
+  public boolean suit_is_greater_than( Card rhs ) {
+    return m_suit.ordinal() > rhs.m_suit.ordinal();
   }
   
   // Compares if the card face value of two cards are equal.  
@@ -62,8 +76,7 @@ class Card {
   //
   // @param[in] rhs   The right hand side card.
   // @return    true if equal.
-  public boolean is_equal( Card rhs )
-  {
+  public boolean value_is_equal( Card rhs ) {
     return m_value.ordinal() == rhs.m_value.ordinal();
   }
   
@@ -72,8 +85,7 @@ class Card {
   //
   // @param[in] rhs   The right hand side card.
   // @return    true if less than.
-  public boolean is_less_than( Card rhs )
-  {
+  public boolean value_is_less_than( Card rhs ) {
     return m_value.ordinal() < rhs.m_value.ordinal();
   }
   
@@ -82,8 +94,7 @@ class Card {
   //
   // @param[in] rhs   The right hand side card.
   // @return    true if less than or equal.
-  public boolean is_less_than_or_equal( Card rhs )
-  {
+  public boolean value_is_less_than_or_equal( Card rhs ) {
     return m_value.ordinal() <= rhs.m_value.ordinal();
   }
   
@@ -92,8 +103,7 @@ class Card {
   //
   // @param[in] rhs   The right hand side card.
   // @return    true if greater than.
-  public boolean is_greater_than( Card rhs )
-  {
+  public boolean value_is_greater_than( Card rhs ) {
     return m_value.ordinal() > rhs.m_value.ordinal();
   }
   
@@ -102,14 +112,32 @@ class Card {
   //
   // @param[in] rhs   The right hand side card.
   // @return    true if greater than or equal.
-  public boolean is_greater_than_or_equal( Card rhs )
-  {
+  public boolean value_is_greater_than_or_equal( Card rhs ) {
     return m_value.ordinal() >= rhs.m_value.ordinal();
+  }
+
+  // Compares if this card is greater than the other card.
+  // First considers the suit, if both cards have the same suit, considers the card value
+  //
+  // @param[in] other_card  The other card.
+  // @return    1  if this card is greater than the other card
+  //            -1 if this card is smaller than the other card
+  //            0  if both cards are equal
+  public int compare_to(Card other_card) {
+    if (suit_is_less_than(other_card)) {
+      return -1;
+    } else if (suit_is_greater_than(other_card)) {
+      return 1;
+    } else if (value_is_less_than(other_card)) {
+      return -1;
+    } else if (value_is_greater_than(other_card)) {
+      return 1;
+    }
+    return 0;
   }
   
   // Prints a single card to std out.
-  public void print()
-  {
+  public void print() {
     System.out.print(card_array[m_suit.ordinal()][m_value.ordinal()]);
   }
   
@@ -117,48 +145,18 @@ class Card {
 	//
 	// @param[in]	value The value of the card.
 	// @param[in]	suit  The value of thd card.
-  public void set_value(Value value, Suit suit)
-  {
+  public void set_value(Value value, Suit suit) {
     m_value = value;
     m_suit = suit;
   }
   
-  public void set_value(int value, int suit)
-  {
+  public void set_value(int value, int suit) {
     m_value = Value.values()[value];
     m_suit = Suit.values()[suit];
   }
  
   
   private static String card_array[][] = {{"[JKR]",
-                                    "[ A\u2660]",
-                                    "[ 2\u2660]",
-                                    "[ 3\u2660]",
-                                    "[ 4\u2660]",
-                                    "[ 5\u2660]",
-                                    "[ 9\u2660]",
-                                    "[ 7\u2660]",
-                                    "[ 8\u2660]",
-                                    "[ 9\u2660]",
-                                    "[10\u2660]",
-                                    "[ J\u2660]",
-                                    "[ Q\u2660]",
-                                    "[ K\u2660]"
-                                  },{"[JKR]",
-                                    "[ A\u2665]",
-                                    "[ 2\u2665]",
-                                    "[ 3\u2665]",
-                                    "[ 4\u2665]",
-                                    "[ 5\u2665]",
-                                    "[ 6\u2665]",
-                                    "[ 7\u2665]",
-                                    "[ 8\u2665]",
-                                    "[ 9\u2665]",
-                                    "[10\u2665]",
-                                    "[ J\u2665]",
-                                    "[ Q\u2665]",
-                                    "[ K\u2665]"
-                                  },{"[JKR]",
                                     "[ A\u2666]",
                                     "[ 2\u2666]",
                                     "[ 3\u2666]",
@@ -185,7 +183,36 @@ class Card {
                                     "[10\u2665]",
                                     "[ J\u2663]",
                                     "[ Q\u2663]",
-                                    "[ K\u2663]"}};
+                                    "[ K\u2663]"
+                                  },{"[JKR]",
+                                    "[ A\u2665]",
+                                    "[ 2\u2665]",
+                                    "[ 3\u2665]",
+                                    "[ 4\u2665]",
+                                    "[ 5\u2665]",
+                                    "[ 6\u2665]",
+                                    "[ 7\u2665]",
+                                    "[ 8\u2665]",
+                                    "[ 9\u2665]",
+                                    "[10\u2665]",
+                                    "[ J\u2665]",
+                                    "[ Q\u2665]",
+                                    "[ K\u2665]"
+                                  },{"[JKR]",
+                                    "[ A\u2660]",
+                                    "[ 2\u2660]",
+                                    "[ 3\u2660]",
+                                    "[ 4\u2660]",
+                                    "[ 5\u2660]",
+                                    "[ 9\u2660]",
+                                    "[ 7\u2660]",
+                                    "[ 8\u2660]",
+                                    "[ 9\u2660]",
+                                    "[10\u2660]",
+                                    "[ J\u2660]",
+                                    "[ Q\u2660]",
+                                    "[ K\u2660]"
+                                  }};
   
   private Suit m_suit;  // The suit of the card
   private Value m_value;  // The value of the card
--- Deck.java
+++ Deck.java
@@ -69,7 +69,80 @@ class Deck
     Collections.shuffle(all_cards, new Random(seed));
   }
   
-  public void sort() 
-  {}
+  // Sorts the list of cards by using merge sort algorithm
+  public void sort() {
+    merge_sort(0, all_cards.size() - 1);
+  }
+
+  /**
+   * Implements the merge sort algorithm for sorting all_cards list
+   *
+   * @param start_index   index of the beginning of the range
+   * @param end_index     index of the end of the range
+   */
+  private void merge_sort(int start_index, int end_index) {
+    if (start_index < end_index) {
+
+      int middle_index = (start_index + end_index) / 2; // finds the middle of the given range
+
+      // sorts the first half of the array
+      merge_sort(start_index, middle_index);
+
+      // sorts the second half of the array
+      merge_sort(middle_index + 1, end_index);
+
+      // merge both halves back
+      merge(start_index, middle_index, end_index);
+    }
+  }
+
+  /**
+   * merges two halves of an all_cards arraylist
+   *
+   * @param start_index   index of the beginning of the range
+   * @param middle_index  index of the middle of the range
+   * @param end_index     index of the end of the range
+   */
+  private void merge(int start_index, int middle_index, int end_index) {
+    // the temp arraylist which will be the sorted list
+    ArrayList<Card> sorted_list = new ArrayList<>();
+
+    int first_index = start_index;
+    int second_index = middle_index + 1;
+
+    // loop over first and second halves
+    // it will stop when either the first half or second half is finished
+    while (first_index <= middle_index && second_index <= end_index) {
+      if (all_cards.get(first_index).compare_to(all_cards.get(second_index)) == -1) { 
+        // first card is smaller than the second card
+        sorted_list.add(all_cards.get(first_index));
+        first_index++;
+      } else { 
+        // second card is smaller than the first card
+        sorted_list.add(all_cards.get(second_index));
+        second_index++;
+      }
+    }
+
+    // This loop will add the remaining elements in the first half of the list (if there is any!)
+    while (first_index <= middle_index) {
+      sorted_list.add(all_cards.get(first_index));
+      first_index++;
+    }
+
+    // This loop will add the remaining elements in the second half of the list (if there is any!)
+    while (second_index <= end_index) {
+      sorted_list.add(all_cards.get(second_index));
+      second_index++;
+    }
+
+    // Writing the sorted arraylist over the all_cards arraylist
+    int i = 0;
+    int j = start_index;
+    while (i < sorted_list.size()) {
+      all_cards.set(j, sorted_list.get(i++));
+      j++;
+    }
+  }
   
 }
